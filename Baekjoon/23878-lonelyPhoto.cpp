6d2393b82362a50a45f04c5112bdd7451b0049c6
Jimmy-Sim <jimmysim2026@gmail.com>
2022-11-09 23:23:30 +0900

    23878 - Lonely Photo

--- Baekjoon/23878-lonelyPhoto.cpp
+++ Baekjoon/23878-lonelyPhoto.cpp
@@ -7,7 +7,7 @@ const int MAX = 500005;
 int N;
 char line[MAX];
 int limit = 3, x;
-int ans;
+long long ans;
 
 int main()
 {
@@ -16,38 +16,15 @@ int main()
     cin >> N;
     for (int i = 0; i < N; i++) cin >> line[i];
 
-    /*while (limit <= N) {
-        for (int i = 0; i <= N - limit; i++) {
-            int gCount = 0, hCount = 0;
+    for (int i = 0; i < N; i++) {
+        long long leftCount = 0, rightCount = 0;
 
-            for (int j = i; j < i + limit; j++) {
-                if (line[j] == 'G') gCount++;
-                else hCount++;
-            }
+        for (int j = i - 1; j >= 0 && line[i] != line[j]; j--) leftCount++;
+        for (int j = i + 1; j < N && line[i] != line[j]; j++) rightCount++;
 
-            if (gCount == 1 || hCount == 1) ans++;
-        }
-
-        limit++;
-    }*/
-
-    for (int i = 0; i <= N - 3; i++) {
-        int gCount = 0, hCount = 0;
-
-        for (int j = i; j < i + 3; j++) {
-            if (line[j] == 'G') gCount++;
-            else hCount++;
-        }
-
-        for (int j = i + 3; j <= N; j++) {
-            if (gCount == 1 || hCount == 1) {
-                ans++;
-
-                if (line[j] == 'G') gCount++;
-                else hCount++;
-            }
-            else break;
-        }
+        ans += leftCount * rightCount;
+        ans += max(0LL, leftCount - 1);
+        ans += max(0LL, rightCount - 1);
     }
 
     cout << ans << "\n";
