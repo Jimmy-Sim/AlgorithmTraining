cdd602cda0e3f5fc3ad6e75ec9b8a7e2c66ad009
Jimmy-Sim <jimmysim2026@gmail.com>
2023-01-08 23:40:15 +0900

    Jan. 8, 2023

---
+++ Baekjoon/11724-연결요소의개수.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> connections[1005];
+int visited[1005];
+
+void dfs(int cur) {
+    visited[cur] = 1;
+
+    
+}
+
+int main()
+{
+    cin >> N >> M;
+}
