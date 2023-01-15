be9c35ca668e4a40a556936c5a9eafcc4bce9087
Jimmy-Sim <jimmysim2026@gmail.com>
2023-01-15 16:42:35 +0900

    Jan. 15, 2023

--- Baekjoon/11724-연결요소의개수.cpp
+++ Baekjoon/11724-연결요소의개수.cpp
@@ -4,16 +4,46 @@
 using namespace std;
 
 int N, M;
-vector<int> connections[1005];
 int visited[1005];
+vector<int> connections[1005];
+int ans;
 
 void dfs(int cur) {
     visited[cur] = 1;
 
-    
+    for (int i = 0; i < connections[cur].size(); i++) {
+        int nxt = connections[cur][i];
+
+        if (!visited[nxt]) dfs(nxt);
+    }
 }
 
 int main()
 {
     cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        connections[a].push_back(b);
+        connections[b].push_back(a);
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (connections[i].size() == 0) {ans++;
+            continue;
+        }
+        for (int j = 0; j < connections[i].size(); j++) {
+            int node = connections[i][j];
+
+            if (!visited[node]) {
+                ans++;
+                dfs(node);
+            }
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
 }
