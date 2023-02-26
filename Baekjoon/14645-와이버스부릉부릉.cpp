db0f87ce1be9a167144eab58e042d77a96f22321
Jimmy-Sim <jimmysim2026@gmail.com>
2023-02-26 16:21:56 +0900

    Feb. 26, 2023

---
+++ Baekjoon/14645-와이버스부릉부릉.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+
+int main()
+{
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        int num1, num2;
+        cin >> num1 >> num2;
+    }
+
+    cout << "비와이\n";
+
+    return 0;
+}
