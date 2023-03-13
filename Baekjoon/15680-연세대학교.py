f5b0c4e41eb8166f1a435ab00d30733955d1683a
Jimmy-Sim <jimmysim2026@gmail.com>
2023-03-13 16:42:23 +0900

    Mar. 13, 2023

---
+++ Baekjoon/15680-연세대학교.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+if number == 0:
+    print("YONSEI")
+else:
+    print("Leading the Way to the Future")
---
+++ Baekjoon/27328-three-WayComparison.py
@@ -0,0 +1,8 @@
+A, B = int(input()), int(input())
+
+if A < B:
+    print(-1)
+elif A == B:
+    print(0)
+else:
+    print(1)
