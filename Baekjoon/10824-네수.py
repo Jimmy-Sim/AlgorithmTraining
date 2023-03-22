a6f4a416c71dcd289ca1d6cce2431ec267d70df7
Jimmy-Sim <jimmysim2026@gmail.com>
2023-03-22 10:58:52 +0900

    Mar. 22, 2023

---
+++ Baekjoon/10824-네수.py
@@ -0,0 +1,5 @@
+a, b, c, d = map(str, input().split())
+
+first, second = a + b, c + d
+
+print(int(first) + int(second))
---
+++ Baekjoon/26768-h4x0r.py
@@ -0,0 +1,9 @@
+phrase = input()
+
+phrase = phrase.replace('a', '4')
+phrase = phrase.replace('e', '3')
+phrase = phrase.replace('i', '1')
+phrase = phrase.replace('o', '0')
+phrase = phrase.replace('s', '5')
+
+print(phrase)
