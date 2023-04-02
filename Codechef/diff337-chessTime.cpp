ba4a3d8a00e8b0fa3eda4c4bff6731e55df5f7f0
Jimmy-Sim <jimmysim2026@gmail.com>
2023-04-02 09:22:20 +0900

    Apr. 2, 2023

---
+++ Codechef/diff337-chessTime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+	    int n;
+	    cin >> n;
+	    
+	    cout << (60 * n) / 20 << '\n';
+	}
+	
+	return 0;
+}
