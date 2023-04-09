a3bb6e073ecb901aa863cd7e21cceb88bc603288
Jimmy-Sim <jimmysim2026@gmail.com>
2023-04-09 11:17:10 +0900

    Apr. 9, 2023

---
+++ Codechef/diff570-chefAndCandies.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+	    int a, b;
+	    cin >> a >> b;
+	    
+	    if (a > b) {
+	        if ((a - b) % 4 != 0) cout << (a - b) / 4 + 1 << '\n';
+	        else cout << (a - b) / 4 << '\n';
+	    }
+	    else cout << 0 << '\n';
+	}
+	
+	return 0;
+}
