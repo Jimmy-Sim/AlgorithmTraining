f1e67ae493fc749b4f0c4c0f6f614850cc2251c0
Jimmy-Sim <jimmysim2026@gmail.com>
2023-04-14 19:55:17 +0900

    Apr. 14, 2023

---
+++ Codechef/diff641-flipTheCards.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--) {
+	    int a, b;
+	    cin >> a >> b;
+	    
+	    cout << min(b, (a - b)) << '\n';
+	}
+	
+	return 0;
+}
