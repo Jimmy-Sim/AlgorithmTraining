04d8cfb27b81ce06ae9ac2ba59cdfeea4998ba53
Jimmy-Sim <jimmysim2026@gmail.com>
2023-06-26 08:19:02 +0900

    Jun. 26, 2023

---
+++ Codechef/diff878-chefOnIsland.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, c, d, e;
+        cin >> a >> b >> c >> d >> e;
+
+        if (a >= c * e && b >= d * e) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
---
+++ Codechef/diff878-richieRich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        cout << (b - a) / c << '\n';
+    }
+
+    return 0;
+}
