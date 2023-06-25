9eafddfdac04e2cc9fe87d20967f6f0b81bff524
Jimmy-Sim <jimmysim2026@gmail.com>
2023-06-25 12:03:55 +0900

    Jun. 25, 2023

---
+++ Codechef/diff872-groupAssignment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        cout << (2 * a + 1) - b << '\n';
+    }
+
+    return 0;
+}
