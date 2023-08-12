09b300743315fc1cebc8c944fc4a7635a906980d
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-08-12 12:12:29 +0900

    Update 443-stringCompression.cpp

--- LeetCode/443-stringCompression.cpp
+++ LeetCode/443-stringCompression.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.length(), m = needle.length();
-        if (m == 0) return 0;
+    int compress(vector<char>& chars) {
+        int ans = 0;
 
-        vector<int> pi(m);
-        pi[0] = 0;
-        int j = 0;
-        for (int i = 1; i < m; i++) {
-            while (j > 0 && needle[j] != needle[i]) j = pi[j - 1];
-            if (needle[j] == needle[i]) j++;
-            pi[i] = j;
-        }
-        
-        j = 0;
-        for (int i = 0; i < n; i++) {
-            while (j > 0 && needle[j] != haystack[i]) j = pi[j - 1];
-            if (needle[j] == haystack[i]) j++;
-            if (j == m) return i - m + 1;
+        for (int i = 0; i < chars.size();) {
+            const char letter = chars[i];
+            int count = 0;
+
+            while (i < chars.size() && chars[i] == letter) {
+                count++;
+                i++;
+            }
+
+            chars[ans++] = letter;
+
+            if (count > 1) {
+                for (const char c : to_string(count)) chars[ans++] = c;
+            }
         }
 
-        return -1;
+        return ans;
     }
 };
