69c4f419c91786c8da62d94b82963e3ec0f2f73d
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-08-22 19:27:38 +0900

    Create 2300-successfulPairsOfSpellsAndPotions.cpp

---
+++ LeetCode/2300-successfulPairsOfSpellsAndPotions.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& s, vector<int>& p, long long suc) {
+        vector<int> v(s.size(), 0);
+        sort(p.begin(), p.end());
+        
+        for (int i = 0; i < s.size(); i++) {
+            int h = p.size() - 1, l = 0, mid;
+            while (l <= h) {
+                mid = l + (h - l) / 2;
+                if ((long long int)s[i] * (long long int)p[mid] >= suc) h = mid - 1;
+                else l = mid + 1;
+            }
+            v[i] = p.size() - 1 - h;
+        }
+        
+        return v;
+    }
+};
