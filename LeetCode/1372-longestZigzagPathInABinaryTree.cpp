31487709a354d59cd4c606f5d32b5e8b0aa9bba7
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-08-26 17:34:08 +0900

    Create 1372-longestZigzagPathInABinaryTree.cpp

---
+++ LeetCode/1372-longestZigzagPathInABinaryTree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int longestZigZag(TreeNode* root) {
+        vector<int> res = dfs(root);
+
+        return res[2];
+    }
+    
+private:
+    vector<int> dfs(TreeNode* node) {
+        if (node == nullptr) return {-1, -1, -1};
+        
+        vector<int> leftSubtree = dfs(node -> left), rightSubtree = dfs(node -> right);
+        
+        int leftLength = leftSubtree[1] + 1, rightLength = rightSubtree[0] + 1, maxLen = max({leftLength, rightLength, leftSubtree[2], rightSubtree[2]});
+        
+        return {leftLength, rightLength, maxLen};
+    }
+};
