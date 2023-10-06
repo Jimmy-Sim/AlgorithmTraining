197a46b57b069060b8c65b62afbd3c2b358699ec
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-10-06 10:01:29 +0900

    Update 486-predictTheWinner.cpp

--- LeetCode/486-predictTheWinner.cpp
+++ LeetCode/486-predictTheWinner.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    bool PredictTheWinner(vector<int>& nums) {
+    bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
+        vector<int> max_diff(n, 0);
+
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (i == j) dp[i][j] = nums[i];
-                else dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
-            }
+            max_diff[i] = nums[i];
+            for (int j = i + 1; j < n; j++) max_diff[j] = max(nums[i] - max_diff[j], nums[j] - max_diff[j - 1]);
         }
-        
-        return dp[0][n - 1] >= 0;
+
+        return max_diff[n - 1] >= 0;        
     }
 };
