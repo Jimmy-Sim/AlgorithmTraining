96d2764c6328fea6ff90fa25e74df8bc4fbc56cb
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-10-26 09:52:23 +0900

    Create 2785-LANCI.cpp

---
+++ Baekjoon/2785-LANCI.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main () {
+    int N;
+    cin >> N;
+    
+    vector<int> lengths;
+    for (int i = 0; i < N; i++) {
+        int length;
+        cin >> length;
+        lengths.push_back(length);
+    }
+    sort(lengths.begin(), lengths.end());
+
+    int cnt = 0;
+    while (lengths.size() > 1) {
+        lengths[lengths.size() - 2] += lengths[lengths.size() - 1];
+        lengths[0]--;
+        cnt++;
+
+        lengths.pop_back();
+
+        if (lengths[0] == 0) {
+            for (int i = 0; i < lengths.size(); i++) lengths[i] = lengths[i + 1];
+            lengths.pop_back();
+        }
+    }
+
+    cout << cnt << '\n';
+}
