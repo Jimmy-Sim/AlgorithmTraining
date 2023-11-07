140bf5f1430aad1c5edd219eb6f4f6f506ee516c
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-11-07 17:18:09 +0900

    Create 68-textJustification.cpp

---
+++ LeetCode/68-textJustification.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> res, curLine;
+        int i = 0, width = 0;
+        while (i < words.size()) {
+            string curWord = words[i];
+            if (width + curWord.length() <= maxWidth) {
+                curLine.push_back(curWord);
+                width += curWord.length() + 1;
+                i++;
+            }
+            else {
+                int spaces = maxWidth - width + curLine.size(), added = 0, j = 0;
+                while (added < spaces) {
+                    if (j >= curLine.size() - 1) j = 0;
+
+                    curLine[j] += " ";
+                    added++;
+                    j++;
+                }
+
+                string line = "";
+                for (const string& word : curLine) line += word;
+                res.push_back(line);
+
+                curLine.clear();
+                width = 0;
+            }
+        }
+
+        for (int word = 0; word < curLine.size() - 1; word++) curLine[word] += " ";
+        curLine[curLine.size() - 1] += string(maxWidth - width + 1, ' ');
+
+        string lastLine = "";
+        for (const string& word : curLine) lastLine += word;
+        res.push_back(lastLine);
+
+        return res;
+    }
+};
