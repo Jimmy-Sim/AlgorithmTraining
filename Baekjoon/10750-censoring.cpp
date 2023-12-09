36f349b808b482d9bda1f2f67b321cddebd622b7
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-12-09 15:21:51 +0900

    Update 10750-censoring.cpp

--- Baekjoon/10750-censoring.cpp
+++ Baekjoon/10750-censoring.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-string phrase, subPhrase, newPhrase;
-
 int main()
 {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
-    cin >> phrase >> subPhrase;
-
-    int num = 0, phraseLength = phrase.size(), subPhraseLength = subPhrase.size();
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     
-    while (num < phraseLength - subPhraseLength) {
-        string substringB = phrase.substr(num, subPhraseLength);
+	string phrase, subPhrase;
+	cin >> phrase >> subPhrase;
 
-        if (substringB == subPhrase) {
-            string substringA = phrase.substr(0, num), substringC = phrase.substr(num + subPhraseLength, phraseLength - (num + subPhraseLength));
-
-            phrase = substringA + substringC;
-
-            if (num < subPhraseLength) {
-                num = 0;
-            }
-            else {
-                num -= subPhraseLength;
-            }
-        }
-        else {
-            num++;
-        }
-        phraseLength = phrase.size();
-    }
-
-    cout << phrase << "\n";
-
-    return 0;
+	string censoredPhrase;
+	for (int i = 0; i < phrase.size(); i++) {
+		censoredPhrase += phrase[i];
+		if (censoredPhrase.size() > subPhrase.size() && censoredPhrase.substr(censoredPhrase.size() - subPhrase.size()) == subPhrase) censoredPhrase.resize(censoredPhrase.size() - subPhrase.size());
+	}
+    
+	cout << censoredPhrase << '\n';
 }
