60028af4fd68c79656d322997b3330664fe9037c
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-12-12 20:11:19 +0900

    Update 1654-랜선자르기.cpp

--- Baekjoon/1654-랜선자르기.cpp
+++ Baekjoon/1654-랜선자르기.cpp
@@ -2,36 +2,34 @@
 
 using namespace std;
 
-const int MAX = 10005;
-
-int N, K;
-int lanCables[MAX], maxCable;
-
 int main()
 {
-    cin >> N >> K;
-    for (int i = 0; i < N; i++) {
-        cin >> lanCables[i];
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    
+    int K, N;
+	cin >> K >> N;
+	
+    int lengths[10005], maxLength = 0;
+	for (int i = 0; i < K; i++) {
+		cin >> lengths[i];
+        maxLength = max(maxLength, lengths[i]);
+	}
+    
+	long long low = 1, high = maxLength;
+	int ans = 0;
+	while (low <= high) {
+		long long mid = (low + high) / 2;
+		int cnt = 0;
+		for (int i = 0; i < K; i++) cnt += lengths[i] / mid;
 
-        maxCable = lanCables[i] > maxCable ? lanCables[i] : maxCable;
-    }
-
-    int length = maxCable / K;
-    while (true) {
-        int count = 0;
-
-        for (int i = 0; i < N; i++) {
-            count += lanCables[i] / length;
-        }
-
-        if (count < K) break;
-
-        length++;
-    }
-
-    length--;
-
-    cout << length << '\n';
-
+		if (cnt >= N) {
+			low = mid + 1;
+			if (ans < mid) ans = mid;
+		}
+		else high = mid - 1;
+	}
+	
+	cout << ans << '\n';
+    
     return 0;
 }
