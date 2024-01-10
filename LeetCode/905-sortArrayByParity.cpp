dc50846edb3f7852b486b592202cc8e6e08b3528
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2024-01-10 17:17:01 +0900

    Create 905-sortArrayByParity.cpp

---
+++ LeetCode/905-sortArrayByParity.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = n - 1;
+
+        while (left < right) {
+            while (left < right && nums[left] % 2 == 0)left++;
+            while (right > left && nums[right] % 2 != 0) right--;
+            if (left < right) swap(nums[left], nums[right]);
+        }
+
+        return nums;
+    }
+};
