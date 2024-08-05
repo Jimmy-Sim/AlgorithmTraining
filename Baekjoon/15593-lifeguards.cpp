1bc779b8f44ad89dc8b420eca5e88969f4b9d599
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2024-08-05 15:28:07 -0700

    Update 15593-lifeguards.cpp

--- Baekjoon/15593-lifeguards.cpp
+++ Baekjoon/15593-lifeguards.cpp
@@ -1,44 +1,71 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
-    int shifts[105][5]; // shifts[index][0]: shift start time; shifts[index][1]: shift end time
-    int timetable[1005] = {}; // initialize all timetable to 0 first (done by the {})
-    for (int i = 0; i < n; i++) {
-        cin >> shifts[i][0] >> shifts[i][1];
+    int N;
+    cin >> N;
 
-        for (int j = shifts[i][0]; j < shifts[i][1]; j++) {
-            timetable[j]++;
-        }
+    // 2D times:
+    /*
+    int times[105][1005] = {}, minStart = 1000, maxEnd = 0;
+    for (int i = 1; i <= N; i++) {
+        int start, end;
+        cin >> start >> end;
+        
+        for (int j = start; j < end; j++) times[i][j] = 1;
+
+        minStart = min(start, minStart);
+        maxEnd = max(end, maxEnd);
     }
 
-    int maxTimeCount = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = shifts[i][0]; j < shifts[i][1]; j++) {
-            timetable[j]--;
-        }
-        int timeCount = 0;
-        for (int j = 0; j <= 1000; j++) {
-            if (timetable[j] >= 1) {
-                timeCount++;
+    int maxCnt = 0;
+    for (int i = 1; i <= N; i++) {
+        int cnt = 0, checked[1005] = {};
+        for (int j = minStart; j < maxEnd; j++) {
+            for (int k = 1; k <= N; k++) {
+                if (k != i && times[k][j] && !checked[j]) {
+                    cnt++;
+                    checked[j] = 1;
+                }
             }
         }
 
-        if (timeCount > maxTimeCount) {
-            maxTimeCount = timeCount;
-        }
-
-        for (int j = shifts[i][0]; j < shifts[i][1]; j++) {
-            timetable[j]++;
-        }
+        maxCnt = max(cnt, maxCnt);
     }
 
-    cout << maxTimeCount << '\n';
+    cout << maxCnt << '\n';
+    */
 
+    // 1D times:
+    int starts[105], ends[105], times[1005] = {}, minStart = 1000, maxEnd = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> starts[i] >> ends[i];
+
+        for (int j = starts[i]; j < ends[i]; j++) times[j]++;
+
+        minStart = min(starts[i], minStart);
+        maxEnd = max(ends[i], maxEnd);
+    }
+
+    int maxCnt = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = starts[i]; j < ends[i]; j++) times[j]--;
+
+        int cnt = 0;
+        for (int j = minStart; j < maxEnd; j++) {
+            if (times[j] > 0) cnt++;
+        }
+
+        maxCnt = max(cnt, maxCnt);
+
+        for (int j = starts[i]; j < ends[i]; j++) times[j]++;
+    }
+
+    cout << maxCnt << '\n';
+    
     return 0;
 }
